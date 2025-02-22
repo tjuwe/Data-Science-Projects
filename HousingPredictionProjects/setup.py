38de6898a15c51bae692e68352b26ab8d6a39f80
e <tjt163787@gmail.com>
2025-02-22 10:21:41 +0000

    Project directory set up

---
+++ HousingPredictionProjects/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using linear regression to predict Price of Houses',
+    author='tj',
+    license='MIT',
+)
